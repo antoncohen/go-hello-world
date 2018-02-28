00efd087d5c5ac0df35f148c9cd7a21f0a915969
Anton Cohen <anton@antoncohen.com>
2018-02-27 18:12:03 -0800

    Update date to Tue Feb 27 18:11:19

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
-	fmt.Fprintln(w, "Updated:", "Fri Feb 23 17:37:50 PST 2018")
+	fmt.Fprintln(w, "Updated:", "Tue Feb 27 18:11:19 PST 2018")
 }
 
 func getEnv(envVar, defaultValue string) string {
